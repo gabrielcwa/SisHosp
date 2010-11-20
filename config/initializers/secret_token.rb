e6184138f9df37bc7bf551836651320bd056917c
Gabriel Cavalcanti <gabriel@lainformatica.com.br>
2010-11-20 01:32:12 -0300

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sishosp::Application.config.secret_token = '2afcf6aabdf817dcd4139c40bc565fa34e3dec677d2bc27842bb697b1a2489a898a4c5f6fe9b4b0de7a9c6eefd5a616559a9473677bb29c5c61a7b8546135815'
